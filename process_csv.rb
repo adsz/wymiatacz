ea3279ec7c8fb06be34295f6bb812aaade066e86
Adam Szydlo <Adam.Szydlo@redbeemedia.com>
2020-02-12 17:19:50 +0000

    Import danych do tablicy

--- process_csv.rb
+++ process_csv.rb
@@ -4,17 +4,33 @@
 # Policzyć ilość dni kiedy giełda była powyżej lub poniżej danej wartości
 
 require 'open-uri'
+require 'csv'
 
 URL="https://stooq.pl/q/d/l/?s=wig&i=d&c=1"
-LOCAL_DIR="./data.csv"
+FILE_PATH="./data.csv"
 
-def download_data_file(dir,url)
-    File.open(dir, "wb") do |file|
-    file.write open(url).read
+class Get_file
+  def download_data_file(file_path,url)
+      File.open(file_path, "wb") do |file|
+        file.write open(url).read
+      end
   end
 end
 
-def import_data_into_array()
+class Import_data
+  def import_data_into_array(file_path)
+    table = CSV.parse(File.read(file_path),col_sep: ";", headers: true)
+    #puts table[0][0]
+  end
 end
 
-download_data_file(LOCAL_DIR,URL)
+class Add_Closing_Price_Difference
+  
+end
+
+#get_file = Get_file.new
+import_data = Import_data.new
+
+#get_file.download_data_file(FILE_PATH,URL)
+imported_data=import_data.import_data_into_array(FILE_PATH)
+puts imported_data[1][1]
