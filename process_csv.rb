aea71d9d399c4ac4f074f4d970c5e3cf732878ff
adsz <adamszydlo@gmail.com>
2020-02-12 11:04:54 +0100

    Start projektu

---
+++ process_csv.rb
@@ -0,0 +1,4 @@
+# Obliczyć różnicę zamkniecia dnia aktualnego do zamknięcia dnia poprzedniego (w %)
+# Posortować dane według różnic zamknięcia
+# Wypisać 5 najlepszych i 5 najgorszych dni
+# Policzyć ilość dni kiedy giełda była powyżej lub poniżej danej wartości
