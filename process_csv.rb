fb9853e0402bedb7261c4fbc446c3e38a88918ec
adsz <adamszydlo@gmail.com>
2020-02-12 11:28:02 +0100

    Sciąganie pliku z danymi

--- process_csv.rb
+++ process_csv.rb
@@ -2,3 +2,19 @@
 # Posortować dane według różnic zamknięcia
 # Wypisać 5 najlepszych i 5 najgorszych dni
 # Policzyć ilość dni kiedy giełda była powyżej lub poniżej danej wartości
+
+require 'open-uri'
+
+URL="https://stooq.pl/q/d/l/?s=wig&i=d&c=1"
+LOCAL_DIR="./data.csv"
+
+def download_data_file(dir,url)
+    File.open(dir, "wb") do |file|
+    file.write open(url).read
+  end
+end
+
+def import_data_into_array()
+end
+
+download_data_file(LOCAL_DIR,URL)
